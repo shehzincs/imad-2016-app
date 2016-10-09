b7b7ca31ac08f133ee8b6bad39f210062e34c2f4
shehzin cs <shehzincs@gmail.com>
2016-10-09 13:24:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+//change the txt
+var element = document.getElmenyById('text');
+element.innerHTML="new value";
+
+//move the image 
+var img = document.getelementById('img');
+img.onclick = function(){
+    img.style.marginleft= '100px';
+    
+};
