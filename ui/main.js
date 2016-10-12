575854872455c61424d952e7d440169095803f95
shehzin cs <shehzincs@gmail.com>
2016-10-12 20:00:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element = document.getElementById('text');
 element.innerHTML="new value";
 
 //move the image 
+var marginLeft=0;
+function moveright() {
+    marginLeft=marginLeft+10
+    img.style.marginLeft=marginLeft+ "px";
+}
 var img = document.getElementById('img');
 img.onclick = function(){
-    img.style.marginLeft= '1000px';
+    //img.style.marginLeft= '1000px';
+    var interval=setInterval(moveright,5);
 };
