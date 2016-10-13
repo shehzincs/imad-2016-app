f48813a409199fe5e6e351823603bf9b6553c975
shehzin cs <shehzincs@gmail.com>
2016-10-13 14:05:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,25 @@ console.log('Loaded!');
 //move the image 
 var marginLeft=0;
 function moveright() {
-    marginLeft=marginLeft+5
+    marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+ "px";
 }
 var img = document.getElementById('img');
 img.onclick = function(){
     //img.style.marginLeft= '1000px';
     var interval=setInterval(moveright,50);
+};
+
+var button = document.getElementById('counter');
+button.onclick = function () {
+  var request = new HTMLhttprequest();
+  request.onreadystatechange = function(){
+    if(request.readystate==HTMLhttprequest.DONE) {
+        if(request.status==200){
+            var counter=request.responseText;
+            var span= document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    } 
+  };
 };
