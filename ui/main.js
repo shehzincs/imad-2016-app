9cbe3553b47c163718931db65c0a465e4c357d89
shehzin cs <shehzincs@gmail.com>
2016-10-13 21:14:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,9 +17,9 @@ img.onclick = function(){
 var button = document.getElementById('counter');
 button.onclick = function () {
   //create a request object
-  var request = new HTMLhttprequest();
+  var request = new XMLHttprequest();
   request.onreadystatechange = function(){
-    if(request.readystate==HTMLhttprequest.DONE) {
+    if(request.readystate==XMLHttprequest.DONE) {
         if(request.status==200){
             var counter=request.responseText;
             var span= document.getElementById('count');
@@ -30,4 +30,18 @@ button.onclick = function () {
   //make a request
   request.open('GET','http://shehzincs.imad.hasura-app.io/counter',true);
   request.send(null);
+};
+
+var nameinput =document.getElementById("name");
+var name = nameinput.value;
+var submit = document.getElementById("submitbtn");
+submit.onclick= function(){
+  
+  var names=["name1","name2","name3"];
+  var list='';
+  for (var i=0;i<names.length;i++){
+      list =='<li>'+name[i]+'</li>';
+  }
+  var ul= document.getElementById('namelist');
+  ul.innerHTML=list;
 };
